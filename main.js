81667e3fd0407963a564886e7f027140a2ec0233
Avantika1306 <77875674+Avantika1306@users.noreply.github.com>
2021-12-03 16:58:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+    
+}
+
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);
+canvas=createCanvas(550,550);
+canvas.position(650,150);
+}
+function draw(){
+    background("white");
+}
+
